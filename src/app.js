a84a73e8b7a32caeaaeb9fb5f04928bc01c6becd
Luis Lugo <124685188+JerryJox@users.noreply.github.com>
2023-07-19 10:37:17 +0000

    funcionalidad ok

--- src/app.js
+++ src/app.js
@@ -1,11 +1,45 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const figura = ["♥", "♦", "♣", "♠"];
+  const unidad = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const dibujoRandom = () => {
+    const figuraRandom = Math.floor(Math.random() * figura.length);
+    let dibujo = figura[figuraRandom];
+    dibujo =
+      dibujo === "♥" || dibujo === "♦"
+        ? '<span class="figura rojo">' + dibujo + "</span>"
+        : '<span class="figura">' + dibujo + "</span>";
+    return dibujo;
+  };
+
+  const numRandom = () => {
+    const unidadRandom = Math.floor(Math.random() * unidad.length);
+    return unidad[unidadRandom];
+  };
+
+  const figuraFinal = dibujoRandom();
+
+  document.getElementById("figuraSup").innerHTML = figuraFinal;
+  document.getElementById("unidades").innerHTML = numRandom();
+  document.getElementById("figuraUpDown").innerHTML = figuraFinal;
 };
